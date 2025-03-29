e2b764adf0640a93c9094a28e0b58d4fb057ec91
hwthangg <hwthang.2510@gmail.com>
2025-03-29 09:04:16 +0700

    implement CRUD User

---
+++ backend/models/UserModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female', 'other'],
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['leader', 'admin'],
+        default: 'leader'
+    },
+    status: {
+        type: String,
+        enum: ['active', 'banned'],
+        default: 'active'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema,  "UserCollection");
