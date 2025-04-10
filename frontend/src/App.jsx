ff34a894ec96119d4e8353904b50685def96c71f
tranphuongVy <22521708@gm.uit.edu.vn>
2025-04-10 21:49:35 +0700

    Searchcomponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TestPage from "./pages/TestPage/TestPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
+import Search from "./components/Search/Search";
 
 function App() {
   return (
   <BrowserRouter>
   <Header/> 
+  <Search/>
     <Routes>
     <Route path="/" element={<TestPage/>} />
     </Routes>
