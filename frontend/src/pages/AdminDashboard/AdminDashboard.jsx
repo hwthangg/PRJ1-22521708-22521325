3b494e2c1675095ab7d4a4a111426c3a956808a3
tranphuongVy <22521708@gm.uit.edu.vn>
2025-04-11 13:06:19 +0700

    updatesocietycomponent

--- frontend/src/pages/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import HistoricalSiteInfo from "../../components/Society/Society";
+import a1 from "../../assets/a1.jpg";
+import a2 from "../../assets/a2.jpg";
+import a3 from "../../assets/a3.jpg";
 
 const AdminDashboard = () => {
-    const site = {
-      name: 'Nhà Tù Phú Lợi',
-      level: 'Quốc gia',
-      images: [
-        '/assets/a1.jpg',
-        '/assets/a1.jpg',
-        '/assets/a1.jpg'
-      ],
+  const sites = [
+    {
+      name: "",
+      images: [a1, a2, a3],
       description:
-      'Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản. Lorem Ipsum có ưu điểm hơn so với đoạn văn bản chỉ gồm nội dung kiểu "Nội dung, nội dung, nội dung" là nó khiến văn bản giống thật hơn, bình thường hơn. Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản. Lorem Ipsum có ưu điểm hơn so với đoạn văn bản chỉ gồm nội dung kiểu. Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản.  Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản.  Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản. '
-    };
-  
-    return (
-      <div>
-        <HistoricalSiteInfo site={site} />
-      </div>
-    );
-  };
-  
-  export default AdminDashboard;
+        "Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản..."
+    },
+    {
+      name: "",
+      images: [a2, a1, a3],
+      description:
+        "Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản..."
+    },
+    {
+      name: "",
+      images: [a3, a2, a1],
+      description:
+        "Chúng ta vẫn biết rằng, làm việc với một đoạn văn bản dễ đọc và rõ nghĩa dễ gây rối trí và cản trở việc tập trung vào yếu tố trình bày văn bản..."
+    }
+  ];
+
+  return (
+    <div>
+      {sites.map((site, index) => (
+        <div key={index} style={{ marginBottom: "40px" }}>
+          <HistoricalSiteInfo site={site} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AdminDashboard;
